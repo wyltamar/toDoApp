c4c3f585d0ae97cc22fa4315e7430b8297a2ff1c
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-05-16 12:55:29 -0300

    Tests ProjectController

--- src/main/java/TodoApp/Main.java
+++ src/main/java/TodoApp/Main.java
@@ -2,6 +2,9 @@
 package TodoApp;
 
 import controller.ProjectController;
+import static java.time.Clock.system;
+import java.util.ArrayList;
+import java.util.List;
 import model.Project;
 
 /**
@@ -14,13 +17,29 @@ public class Main {
     
         ProjectController projectController = new ProjectController();
         
-        Project project = new Project();
-        project.setName("Comanda");
-        project.setDescription("Informatizar comandas");
-        
-        
-        projectController.save(project);
-        
+//        Project project = new Project();
+//        project.setName("Comanda");
+//        project.setDescription("Informatizar comandas");
+//        
+//        
+//        projectController.save(project);
+
+//          Project project = new Project();
+//          project.setName("Comanda Eletrônica");
+//          project.setDescription("Informatizar comandas");
+//          project.setId(3);
+          
+//          projectController.update(project);
+          
+          
+//          List<Project> projects = new ArrayList<Project>();
+//          
+//          projects = projectController.getAll();
+//          
+//          System.out.println("Total de projetos: "+projects.size());
+//        
+
+//            projectController.removeById(1);
     }
     
 }
